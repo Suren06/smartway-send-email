d82072ad51aceb775653f7ba2b117cf3342a22e6
Surendra babu Boyilla <surendrababu.boyilla@pccw.com>
2021-04-18 11:34:59 +0800

    First Commit by Suren

---
+++ src/main/java/com/smartway/smartwaysendemail/SmartwaySendEmailApplication.java
@@ -0,0 +1,13 @@
+package com.smartway.smartwaysendemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwaySendEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwaySendEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartway/smartwaysendemail/SmartwaySendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartway.smartwaysendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwaySendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
